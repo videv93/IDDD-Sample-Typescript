207166a5f028d82b0c37630ca2d4e153c1e033b1
videv93 <videv93@gmail.com>
2023-01-01 11:38:24 +0700

    fix: update calendar entry application service

--- src/collaboration/application/calendar/calendar-entry-application.service.ts
+++ src/collaboration/application/calendar/calendar-entry-application.service.ts
@@ -1,25 +1,40 @@
-import { CalendarEntryId } from "src/collaboration/domain/model/calendar/calendar-entry-id";
-import { CalendarEntryRepository } from "src/collaboration/domain/model/calendar/calendar-entry.repository";
-import { CollaboratorService } from "src/collaboration/domain/model/collaborator/collaborator.service";
-import { Tenant } from "src/collaboration/domain/model/tenant/tenant";
+import { CalendarEntryId } from 'src/collaboration/domain/model/calendar/calendar-entry-id';
+import { CalendarEntryRepository } from 'src/collaboration/domain/model/calendar/calendar-entry.repository';
+import { CollaboratorService } from 'src/collaboration/domain/model/collaborator/collaborator.service';
+import { Participant } from 'src/collaboration/domain/model/collaborator/participant';
+import { Tenant } from 'src/collaboration/domain/model/tenant/tenant';
 
 export class CalendarEntryApplicationService {
   private _calendarEntryRepository: CalendarEntryRepository;
   private _collaboratorService: CollaboratorService;
 
-  constructor(calendarEntryRepository: CalendarEntryRepository, collaboratorService: CollaboratorService) {
+  constructor(
+    calendarEntryRepository: CalendarEntryRepository,
+    collaboratorService: CollaboratorService,
+  ) {
     this._calendarEntryRepository = calendarEntryRepository;
     this._collaboratorService = collaboratorService;
   }
 
-  changeCalendarEntryDescription(tenantId: string, calendarEntryId: string, description: string) {
+  changeCalendarEntryDescription(
+    tenantId: string,
+    calendarEntryId: string,
+    description: string,
+  ) {
     const tenant = new Tenant(tenantId);
-    let calendarEntry = this.calendarEntryRepository.calendarEntryOfId(tenant, new CalendarEntryId(calendarEntryId));
+    let calendarEntry = this.calendarEntryRepository.calendarEntryOfId(
+      tenant,
+      new CalendarEntryId(calendarEntryId),
+    );
     calendarEntry.changeDescription(description);
     this.calendarEntryRepository.save(calendarEntry);
   }
 
-  inviteCalendarEntryParticipant(tenantId: string, calendarEntryId: string, participantsToInvite: Set<string>) {
+  inviteCalendarEntryParticipant(
+    tenantId: string,
+    calendarEntryId: string,
+    participantsToInvite: Set<string>,
+  ) {
     const tenant = new Tenant(tenantId);
     const calendarEntry = this.calendarEntryRepository.calendarEntryOfId(
       tenant,
@@ -32,7 +47,11 @@ export class CalendarEntryApplicationService {
     this.calendarEntryRepository.save(calendarEntry);
   }
 
-  relocateCalendarEntry(tenantId: string, calendarEntryId: string, location: string) {
+  relocateCalendarEntry(
+    tenantId: string,
+    calendarEntryId: string,
+    location: string,
+  ) {
     const tenant = new Tenant(tenantId);
     const calendarEntry = this.calendarEntryRepository.calendarEntryOfId(
       tenant,
@@ -45,5 +64,21 @@ export class CalendarEntryApplicationService {
 
   get calendarEntryRepository() {
     return this._calendarEntryRepository;
+  }
+
+  get collaboratorService() {
+    return this._collaboratorService;
+  }
+
+  private inviteesFrom(tenant: Tenant, participantsToInvite: Set<string>) {
+    let invitees = new Set<Participant>();
+    for (let participantId of participantsToInvite) {
+      let participant = this.collaboratorService.participantFrom(
+        tenant,
+        participantId,
+      );
+      invitees.add(participant);
+    }
+    return invitees;
   }
 }
