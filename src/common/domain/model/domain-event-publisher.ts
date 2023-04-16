4eb3e9f5211e7bb19be3b4d1c4ef848f27f2a4ca
videv93 <videv93@gmail.com>
2023-04-16 12:08:28 +0700

    feat: update domain event publisher

--- src/common/domain/model/domain-event-publisher.ts
+++ src/common/domain/model/domain-event-publisher.ts
@@ -1,3 +1,4 @@
+import { DomainEvent } from '.';
 import { DomainEventSubscriber } from './domain-event-subscriber';
 
 export class DomainEventPublisher {
@@ -6,21 +7,25 @@ export class DomainEventPublisher {
 
   private _publishing: boolean;
 
-  private _subscribers: [];
+  private _subscribers: Array<DomainEventSubscriber<any>>;
 
   public static instance(): DomainEventPublisher {
     return DomainEventPublisher._instance;
   }
 
+  constructor() {
+    this.publishing = false;
+    this.ensureSubscriberList();
+  }
+
   public publish<T>(event: T): void {
-    if (!this.isPublishing && this.hasSubscriber) {
+    if (!this.isPublishing() && this.hasSubscribers()) {
       try {
         this.publishing = true;
         const eventType = event.constructor.name;
         const allSubscribers: DomainEventSubscriber<T>[] = this.subscribers;
         allSubscribers.forEach(subscriber => {
           const subscribedToType = subscriber.subscribedToEventType();
-          // || subscribedToType === DomainEvent.constructor.name
           if (eventType === subscribedToType) {
             subscriber.handleEvent(event);
           }
@@ -31,19 +36,43 @@ export class DomainEventPublisher {
     }
   }
 
+  publishAll(domainEvents: DomainEvent[]) {
+    for (const domainEvent of domainEvents) {
+      this.publish(domainEvent);
+    }
+  }
+
+  subscribe<T>(subscriber: DomainEventSubscriber<T>): void {
+    if (!this.isPublishing()) {
+      this.ensureSubscriberList();
+
+      this.subscribers.push(subscriber);
+    }
+  }
+
+  private ensureSubscriberList() {
+    if (!this.hasSubscribers) {
+      this.subscribers = [];
+    }
+  }
+
   isPublishing(): boolean {
     return this._publishing;
+  }
+
+  hasSubscribers(): boolean {
+    return this.subscribers != null;
   }
 
   set publishing(flag: boolean) {
     this._publishing = flag;
   }
 
-  get hasSubscriber(): boolean {
-    return this.subscribers != null;
-  }
-
   get subscribers() {
     return this._subscribers;
+  }
+
+  set subscribers(subscribers: Array<DomainEventSubscriber<any>>) {
+    this._subscribers = subscribers;
   }
 }
